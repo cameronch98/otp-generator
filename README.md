e01541fc1b07db9f0c89d0c2a35e426e35a976b6
Cameron Hester <cameron12.ch@gmail.com>
2024-05-19 12:58:08 -0400

    first draft of finished product

---
+++ README.md
@@ -0,0 +1,91 @@
+# One Time Password Generator
+
+An application to generate QR codes for use in Google Authenticator and display the associated one time passwords in the terminal. The one time passwords are timed one time passwords on a 30 second timer. The application utilizes the qrcode and pyotp packages to generate the QR codes and display one time passwords. Multiple user support is included, although it isn't intended to be secure as secrets are listed in a readable JSON file. Typer was used to implement the CLI.
+
+# Setup
+
+### MacOS/Linux
+
+First, clone the repository or download/extract the files. Then, it is recommended to go to the project directory and create a virtual environment as follows: 
+
+```
+python<version> -m venv <name-for-venv>
+```
+
+Then, we need to activate the venv by running:
+
+```
+source <name-for-venv>/bin/activate
+```
+
+Now that the virtual environment is enabled in the terminal, we need to install the packages required by the program that reside in requirements.txt. To do so, we can run the following command:
+
+```
+pip install -r requirements.txt
+```
+
+# Usage
+
+### General Info
+
+To get general info about the options and commands available, we can run the program with no arguments:
+
+```
+python otp.py
+```
+```
+python3 otp.py
+```
+```
+<name-for-venv>/bin/python otp.py
+```
+
+NOTE: we've shown three different ways to call the program here. From here on out, we will use python3 to demonstrate the program.
+
+### Generate QR Code
+
+To generate a QR code for an example email/user, we can use one of the following commands:
+
+```
+python3 otp.py generate-qr
+```
+
+To generate a QR code for a specific email/user, we can use one of the following commands:
+
+```
+python3 otp.py generate-qr <email>
+```
+
+### Retrieve One-Time Password
+
+To retrieve a one time password for the example email/user, we can use one of the following commands:
+
+```
+python3 otp.py get-otp
+```
+
+To retrieve a one time password for a specific email/user, we can use one of the following commands:
+
+```
+python3 otp.py get-otp <email>
+```
+
+To repeat the display of one time passwords every 30 seconds until the user quits, add the --repeat option:
+
+```
+python3 otp.py get-otp <email> --repeat
+```
+
+### Help
+
+To get help for any command or the program in general, use the --help option:
+
+```
+python3 otp.py --help
+```
+```
+python3 otp.py generate-qr --help
+```
+```
+python3 otp.py get-otp --help
+```
---
+++ otp.py
@@ -0,0 +1,107 @@
+import os
+import typer
+import pyotp
+import qrcode
+import json
+import time
+from typing import Optional
+from typing_extensions import Annotated
+
+
+app = typer.Typer(
+    no_args_is_help=True,
+    help="Tool to generate QR codes for use in Google Authenticator and display the OTPs created.",
+)
+
+
+@app.command()
+def generate_qr(email: Annotated[Optional[str], typer.Argument()] = None):
+    """
+    Generate QR code for user with EMAIL.
+
+    If email not provided, will default to example@example.com.
+    """
+    # Default to example email if not provided
+    email = "example@example.com" if not email else email
+
+    # Retrieve or generate secret key for URI creation
+    if not (secret := get_secret(email)):
+        secret = create_secret(email)
+
+    # Generate URI
+    uri = pyotp.totp.TOTP(secret).provisioning_uri(name=email, issuer_name="OTP App")
+
+    # Generate QR code and display
+    qr = qrcode.make(uri)
+    qr.save(f"qrcodes/{email}.png")
+    qr.show()
+
+
+@app.command()
+def get_otp(
+    email: Annotated[Optional[str], typer.Argument()] = None,
+    repeat: Annotated[
+        Optional[bool], typer.Option(help="Continue generating OTPs until user quits.")
+    ] = False,
+):
+    """
+    Display OTP for user with EMAIL, optionally with a --repeat.
+
+    If email not provided, will default to example@example.com.
+    If --repeat is used, repeat display of OTP every 30 secs.
+    """
+    # Default to example email if not provided
+    email = "example@example.com" if not email else email
+
+    # Retrieve secret key for user
+    if not (secret := get_secret(email)):
+        print(
+            f"User with email {email} must first generate a QR code using the generate-qr command."
+        )
+        return
+
+    # Display OTPs
+    totp = pyotp.TOTP(secret)
+    print(totp.now())
+    while repeat:
+        time.sleep(30)
+        print(totp.now())
+
+
+def get_secret(email: str):
+    """Search for secret key for a user with the given email"""
+    # Load user database
+    with open("users.json", "r") as f:
+        users = json.load(f)
+
+    # Search for user secret
+    for user in users:
+        if user["email"] == email:
+            return user["secret"]
+
+
+def create_secret(email: str):
+    """Creates secret for a user and adds to JSON file"""
+    # Create secret and user object
+    secret = pyotp.random_base32()
+    user = {"email": email, "secret": secret}
+
+    # Add to JSON file
+    with open("users.json", "r") as f:
+        users = json.load(f)
+    with open("users.json", "w") as f:
+        users.append(user)
+        json.dump(users, f)
+
+    return secret
+
+
+if __name__ == "__main__":
+    # Initialize JSON file if not already done
+    if not os.path.exists("./users.json") and not os.path.exists("./qrcodes"):
+        os.mkdir("./qrcodes")
+        with open("users.json", "w") as f:
+            f.write("[]")
+
+    # Run application
+    app()
